d68028b9d196a2b84f63a3fcff25b8a6dfb42726
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2025-08-28 13:59:07 -0500

    chore: Bump version to 0.2.0

--- ridge_detector/__init__.py
+++ ridge_detector/__init__.py
@@ -1,5 +1,6 @@
+from .detector import RidgeData as RidgeData
 from .detector import RidgeDetector as RidgeDetector
 from .detector import RidgeDetectorConfig as RidgeDetectorConfig
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 name = "ridge_detector"
